852a076ebde36c37b18afe7673742cfe739fa47e
Victoria Reed <vreed1@tigers.lsu.edu>
2013-02-24 16:03:24 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IEEEWebsite::Application.config.secret_token = '87dec4dd0249418b19b6226c426a789a78ede65c48e30813a42cec506691794fb40136e8905367a31d0132ae3bc625336dc1a4a434960bd549394df95379f81b'
